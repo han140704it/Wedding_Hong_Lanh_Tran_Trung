587cf639b38a31c69b9b497eb47f8aae47971338
herin26it <phuonglinh2612004@gmail.com>
2025-12-22 13:16:13 +0700

    first commit

---
+++ src/utils/mock_data.js
@@ -0,0 +1,45 @@
+
+export const WEDDING_DATA = {
+    guestName: "Khách Quý",
+    time: "18 GIỜ 00",
+    day: "THỨ BẢY",
+    date: "10.01.2026",
+    lunarDate: "22 tháng 11 năm Ất Tỵ",
+    eventDate: 10, 
+    
+    location: {
+        name: "TƯ GIA NHÀ CÔ DÂU",
+        address: "Thôn Quảng Tân - Xã Ea Phê - Tỉnh Đắk Lắk",
+        mapLink: "https://www.google.com/maps?q=12.668493,108.367539" 
+    },
+    
+    bankInfo: {
+        bankName: "Ngân hàng MB",
+        accountNumber: "689199877777 ",
+        accountName: "Trần Văn Trung",
+        qrCode: "/assets/account/1.png" 
+    },
+
+    loveStory: {
+    topQuote: "Có lẽ thế gian này có vô vàn điều tươi đẹp, Nhưng trong lòng em, đẹp nhất vẫn chỉ có anh.",
+    // Ảnh nền phía sau (ảnh biển)
+    mainPhoto: "/assets/couple/3.jpg",
+    cutoutPhoto: "/assets/couple/1.png", 
+    bottomQuotes: [
+        "Anh mong những năm tháng sau này. Dù khó khăn hay hạnh phúc, già đi hay trẻ mãi. Em vẫn sẽ luôn bên anh và yêu như ngày đầu tiên.",
+    ]
+  },
+
+    assets: {
+        couplePhoto: "/assets/couple/1.jpg",
+        couplePhoto1: "/assets/couple/3.jpg",
+        couplePhoto2: "/assets/couple/8.jpg",
+        couplePhoto3: "/assets/couple/5.jpg",
+        couplePhoto4: "/assets/couple/2.jpg",
+        couplePhoto5: "/assets/couple/7.jpg",
+        couplePhoto6: "/assets/couple/2.jpg",
+        flowerTop: "https://i.imgur.com/vHqX3p1.png",
+        flowerBottom: "https://i.imgur.com/vHqX3p1.png",
+        giftIcon: "https://cdn-icons-png.flaticon.com/512/4213/4213652.png"
+    }
+};
